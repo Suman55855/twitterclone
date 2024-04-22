32018298c37a039f461228938c42d0048e9d273e
Suman55855 <125995684+Suman55855@users.noreply.github.com>
2024-04-23 03:24:12 +0530

    update

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import Avatar from 'react-avatar'
 
 const Tweet = () => {
   return (
-    <div>Tweet comes here</div>
+    <div>
+      <div>
+      <div className='flex p'>
+              <Avatar
+                src="https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
+                size="40"
+                round={true}
+              />
+              <div className='ml-2'>
+              <div className='flex items-center'>
+                <h1 className='font-bold'>Patel</h1>
+                <p className='text-gray-500 text-sm ml-1'>@patelmernstack . 1m</p>
+              </div>
+              <div>
+                <p>Hello developers lets connect and grow together.</p>
+              </div>
+              </div>
+            </div>
+      </div>
+    </div>
   )
 }
 
